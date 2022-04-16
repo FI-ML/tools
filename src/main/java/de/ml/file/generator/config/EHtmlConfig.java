1f90115c875d00577ed87b310c6af00a6c6e1683
Max <dkzt.kontakt@gmail.com>
2022-04-16 13:31:47 +0200

    add html viewport

--- src/main/java/de/ml/file/generator/config/EHtmlConfig.java
+++ src/main/java/de/ml/file/generator/config/EHtmlConfig.java
@@ -15,6 +15,7 @@ public enum EHtmlConfig {
     HEAD_OPEN_TAG("<head>"),
     HEAD_CLOSE_TAG("</head>"),
     META_TAG("<meta charset=\"utf-8\">"),
+    META_VIEWPORT("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"),
     TITLE_OPEN_TAG("<title>"),
     TITLE_CLOSE_TAG("</title>"),
     CSS_LINK(String.format("%s%s%s%s", "<link rel=\"stylesheet\" type=\"text/css\" href=\"css", "/", "styles.css", "\">")),
